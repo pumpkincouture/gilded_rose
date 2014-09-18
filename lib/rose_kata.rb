e3a65ab4340a93f8f82f00efb870ec3801c8644e
pumpkincouture <pumpkincouture@gmail.com>
2014-09-18 07:49:11 -0500

    Changes 9-17

---
+++ lib/rose_kata.rb
@@ -0,0 +1,3 @@
+class GildedRose
+
+end
---
+++ spec/rose_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/rose_kata.rb'
+require_relative 'spec_helper.rb'
+
+
+describe GildedRose do
+
+  describe "#convert" do
+
+	  before :each do
+		  @gilded_rose = GildedRose.new
+		end
+
+end
