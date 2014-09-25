f1fb854b53ec8b82d6663f2c3793d722516b9cde
Taryn Sauer <taryn.sauer@gmail.com>
2014-09-24 19:56:11 -0500

    Fixes file indentation

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,19 +1,18 @@
 require_relative 'spec_helper'
 
-
 describe GildedRose do
 
   describe "#update_quality" do
 
-	  before :each do
-		  @rose = GildedRose.new
-		  @items = @rose.items
-		end
+    before :each do
+      @rose = GildedRose.new
+      @items = @rose.items
+    end
 
-		it "updates items after one day" do
+    it "updates items after one day" do
       expect(1).to eq(2)
-		end
+    end
 
-	end
+  end
 
 end
