0861836283b4c9e756a5f6cf2f98ed0fa910d430
Taryn Sauer <taryn.sauer@gmail.com>
2014-09-26 15:13:27 -0500

    Adds tests for 1-day and 5-day time lapse

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -9,10 +9,52 @@ describe GildedRose do
       @items = @rose.items
     end
 
-    it "updates items after one day" do
-      expect(1).to eq(2)
+    it "updates sell in  after one day" do
+      @rose.update_quality
+      @items.zip([9, 1, 4, 0, 14, 2]).each do |item, sell_in|
+        expect(item.sell_in).to eq(sell_in), "Expected sell in of #{sell_in} for #{item.name}, but got #{item.sell_in}"
+      end
     end
 
-  end
+    it "updates quality after one day" do
+      @rose.update_quality
+      @items.zip([19, 1, 6, 80, 21, 5]).each do |item, quality|
+        expect(item.quality).to eq(quality)
+      end
+    end
 
+    it "updates sell in after 5 days" do
+      5.times { @rose.update_quality }
+      @items.zip([5, -3, 0, 0, 10, -2]).each do |item, sell_in|
+        expect(item.sell_in).to eq(sell_in), "Expected sell in of #{sell_in} for #{item.name}, but got #{item.sell_in}"
+
+      end
+    end
+    it "updates quality after 5 days" do
+      5.times { @rose.update_quality}
+      assert_quality_updates([15, 8, 2, 80, 25, 0])
+    end
+  end
+end
+
+def assert_quality_updates(quality_values)
+  @items.zip(quality_values).each do |item, quality|
+       expect(item.quality).to eq(quality), "Expected quality of #{quality} for #{item.name}, but got #{item.quality}"
+  end
+end
+
+def get_sell_ins
+  vals = []
+  @items.each do |item|
+    vals << item.sell_in
+  end
+  p  vals
+end
+
+def get_quality
+  quality_val = []
+  @items.each do |item|
+    quality_val << item.quality
+  end
+  p quality_val
 end
