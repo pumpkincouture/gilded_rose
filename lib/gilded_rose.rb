c9b32c80bcfa95b99e87bd9a633397b58bc8e5df
Taryn Sauer <taryn.sauer@gmail.com>
2014-10-03 16:48:57 -0500

    Refactored to add Item methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,48 +17,33 @@ class GildedRose
   end
 
   def update_quality
+    @items.each do |item|
+      unless item.name == "Sulfuras, Hand of Ragnaros"
+        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+          item.decrease_quality
+        else
+          item.increment_quality
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              item.increment_quality
+            end
+            if item.sell_in < 6
+              item.increment_quality
+            end
+          end
+        end
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
-          end
-        end
-      else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-          end
-        end
-      end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
-      end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
-              end
+        item.update_sellin
+
+        if item.sell_in < 0
+          if item.name != "Aged Brie"
+            if item.name != "Backstage passes to a TAFKAL80ETC concert"
+              item.decrease_quality
+            else
+              item.quality = 0
             end
           else
-            @items[i].quality = @items[i].quality - @items[i].quality
-          end
-        else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+            item.increment_quality
           end
         end
       end
--- lib/item.rb
+++ lib/item.rb
@@ -1,12 +1,22 @@
 class Item
 
   attr_accessor :name, :sell_in, :quality
-        
+
   def initialize (name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
 
+  def increment_quality
+    @quality += 1 if @quality < 50
+  end
 
+  def decrease_quality
+    @quality -= 1 if @quality > 0
+  end
+
+  def update_sellin
+    @sell_in -= 1
+  end
 end
