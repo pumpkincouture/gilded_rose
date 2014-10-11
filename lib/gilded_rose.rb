f92c326a5d47906b6e89d6cf561fa5bc21ea413f
pumpkincouture <pumpkincouture@gmail.com>
2014-10-11 18:39:00 -0500

    Updated gilded rose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,18 +9,18 @@ class GildedRose
   def initialize
     @item_factory = ItemFactory.new
     @items = []
-    @items << ItemFactory.create_item("+5 Dexterity Vest", 10, 20)
-    @items << ItemFactory.create_item("Aged Brie", 2, 0)
-    @items << ItemFactory.create_item("Elixir of the Mongoose", 5, 7)
-    @items << ItemFactory.create_item("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << ItemFactory.create_item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << ItemFactory.create_item("Conjured Mana Cake", 3, 6)
+    @items << @item_factory.create_item("+5 Dexterity Vest", 10, 20)
+    @items << @item_factory.create_item("Aged Brie", 2, 0)
+    @items << @item_factory.create_item("Elixir of the Mongoose", 5, 7)
+    @items << @item_factory.create_item("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << @item_factory.create_item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << @item_factory.create_item("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
     @items.each do |item|
-      item.update_quality
       item.update_sellin
+      item.update_quality
     end
   end
 end
