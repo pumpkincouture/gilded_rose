65099298c6a76e7caaadec3589eb6c414c75ab0c
pumpkincouture <pumpkincouture@gmail.com>
2014-10-11 18:39:43 -0500

    Updated sulfuras item class

--- lib/sulfuras_item.rb
+++ lib/sulfuras_item.rb
@@ -2,9 +2,11 @@ require_relative 'item'
 
 class SulfurasItem < Item
 
-	def update_quality		
+	def update_quality	
+		@quality = @quality	
 	end
 
 	def update_sellin
+		@sellin = @sellin
 	end 
 end
