b9633f816b656806eba84430093b0728073c1ab9
pumpkincouture <pumpkincouture@gmail.com>
2014-10-11 18:39:20 -0500

    Updated item factory class

--- lib/item_factory.rb
+++ lib/item_factory.rb
@@ -5,16 +5,16 @@ require_relative 'standard_item'
 
 class ItemFactory
 
-	def create_item(name, sellin, quality)
+	def create_item(name, sell_in, quality)
 		case name
-		when name == "Sulfuras, Hand of Ragnaros"
-		  SulfurasItem.new(name, sellin, quality)
-		when name == "Aged Brie"
-			BrieItem.new(name, sellin, quality)
-		when name == "Backstage passes to a TAFKAL80ETC concert"
-			ConcertItem.new(name, sellin, quality)
+		when "Sulfuras, Hand of Ragnaros"
+		  SulfurasItem.new(name, sell_in, quality)
+		when "Aged Brie"
+			BrieItem.new(name, sell_in, quality)
+		when "Backstage passes to a TAFKAL80ETC concert"
+			ConcertItem.new(name, sell_in, quality)
 		else
-			StandardItem.new(name, sellin, quality)
+			StandardItem.new(name, sell_in, quality)
 		end
 	end
 end
