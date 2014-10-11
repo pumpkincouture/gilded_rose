a48612b2328dd31733b98bbc7356d217ff6548cb
pumpkincouture <pumpkincouture@gmail.com>
2014-10-11 18:38:38 -0500

    Updated concert item class

--- lib/concert_item.rb
+++ lib/concert_item.rb
@@ -3,12 +3,14 @@ require_relative 'item'
 class ConcertItem < Item
 
 	def update_quality
-		if @sell_in <= 10
+		if @sell_in 10
 			@quality += 2
-		elsif @sell_in <= 5
+		elsif @sell_in.between?(1, 5)
 			@quality += 3
-		elsif @sellin <= 0
+		elsif @sell_in <= 0
 			@quality = 0
+		else
+			@quality += 1
 		end
 	end
 
