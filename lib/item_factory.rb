59cdcc850d22c45c590960a3b7f1c3bd2f28857e
pumpkincouture <pumpkincouture@gmail.com>
2014-10-13 11:35:19 -0500

    Added logic for conjured items

--- lib/item_factory.rb
+++ lib/item_factory.rb
@@ -2,17 +2,20 @@ require_relative 'sulfuras_item'
 require_relative 'concert_item'
 require_relative 'brie_item'
 require_relative 'standard_item'
+require_relative 'conjured_item'
 
 class ItemFactory
 
 	def create_item(name, sell_in, quality)
-		case name
-		when "Sulfuras, Hand of Ragnaros"
+	  case 
+		when name == "Sulfuras, Hand of Ragnaros"
 		  SulfurasItem.new(name, sell_in, quality)
-		when "Aged Brie"
+		when name == "Aged Brie"
 			BrieItem.new(name, sell_in, quality)
-		when "Backstage passes to a TAFKAL80ETC concert"
+		when name == "Backstage passes to a TAFKAL80ETC concert"
 			ConcertItem.new(name, sell_in, quality)
+		when name.downcase.match("conjured") 
+			ConjuredItem.new(name, sell_in, quality)
 		else
 			StandardItem.new(name, sell_in, quality)
 		end
